f8c0c0969b169affe1bc22407ce45b7d52e2699d
Cryos <cryos@stiltfox.com>
2023-03-05 01:15:16 -0500

    adding extra tests. they are failing but will fix them later.

--- modules/httpmessage/HttpMessageTest.cpp
+++ modules/httpmessage/HttpMessageTest.cpp
@@ -89,6 +89,42 @@ TEST(HttpMessage, reading_from_a_socket_that_does_not_have_headers_or_a_body_wil
     ASSERT_EQ(actual, HttpMessage(HttpMessage::POST, "/an_endpoint"));
 }
 
+TEST(HttpMessage, reading_from_a_socket_that_does_not_have_a_Body_will_parse_properly)
+{
+    //given we have an http request without a body
+    std::string dataToStream = "POST /an_endpoint HTTP/1.1\r\nheader2: some_val2\r\nheader: some_value";
+    int currentChunk = 0;
+
+    //when we read the socket, let's pretend it's 9080
+    HttpMessage actual(9080,[dataToStream, &currentChunk](int socketId, char* buffer, int chunksize)
+    {
+        int readBytes = getDataChunk(dataToStream, buffer, chunksize, currentChunk);
+        currentChunk++;
+        return readBytes;
+    });
+
+    //then we get back the desired http response
+    ASSERT_EQ(actual, HttpMessage(HttpMessage::POST, "/an_endpoint", {{"header2","some_val2"},{"header","some_value"}}));
+}
+
+TEST(HttpMessage, reading_from_a_socket_that_does_not_have_headings_will_parse_properly)
+{
+    //given we have an http request without headers
+    std::string dataToStream = "POST /an_endpoint HTTP/1.1\r\n\r\nThis is a body";
+    int currentChunk = 0;
+
+    //when we read the socket, let's pretend it's 9080
+    HttpMessage actual(9080,[dataToStream, &currentChunk](int socketId, char* buffer, int chunksize)
+    {
+        int readBytes = getDataChunk(dataToStream, buffer, chunksize, currentChunk);
+        currentChunk++;
+        return readBytes;
+    });
+
+    //then we get back the desired http response
+    ASSERT_EQ(actual, HttpMessage(HttpMessage::POST, "/an_endpoint", {}, "This is a body"));
+}
+
 TEST(HttpMessage, reading_from_an_invalid_socket_request_will_not_cause_a_seg_fault)
 {
     //given we have a garbage data stream
