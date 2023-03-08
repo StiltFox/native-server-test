8c36229bf9966ef6087645f87bb407e913e43a49
Cryos <cryos@stiltfox.com>
2023-03-08 17:16:00 -0500

    wip multithreading.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
+#include <thread>
+#include <mutex>
 #include "Socket.hpp"
 
 using namespace std;
 const HttpMessage KILL_MESSAGE(HttpMessage::DELETE,"/non_public_uri",{{"host", "the_scp_foundation"},{"operation","kill"}}, "死神");
+mutex consoleWriteMutex;
+
+void listenToConnection(Connection*& connection)
+{
+	HttpMessage request = connection->blockingReceiveData();
+	HttpMessage msg(200,{{"content-type","application/json"}},"{\"message\":\"You sent a " + request.getHttpMethodAsString() + " request!\"}");
+	connection->sendData(msg);
+	delete connection;
+	connection = nullptr;
+
+	lock_guard<mutex> guard(consoleWriteMutex);
+	cout << request.printAsRequest() << endl 
+	<< "-------------------------" << endl
+	<< msg.printAsResponse()<<endl
+	<< "-------------------------" << endl;
+}
 
 int main(int argc, char const* argv[])
 {
 	bool shutdown = false;
 	Socket listeningSocket(8080);
 	listeningSocket.listenPort();
+	Connection* listeners[10] = {nullptr};
 		
 	while (!shutdown)
 	{
